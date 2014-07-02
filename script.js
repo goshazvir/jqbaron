c0eaea91b0b473ee36cf45847871dd313569260f
Georgiy <goshazvir@gmail.com>
2014-07-02 17:08:10 +0300

    load

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    window.dima = baron({
+        root: '.wrapper_1',
+        scroller: '.scroller',
+        bar: '.scroller__bar',
+        barOnCls: 'baron'
+    });
+};
